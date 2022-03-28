a4028c04a635307f531a2295eed9664f14bc5452
YoshiCity <pawel.mirys@gmail.com>
2022-03-28 20:06:20 +0200

    Category component with diffrent variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import './App.scss';
+import { Category } from './app/components/Category/Category';
 
 function App() {
-  return <div className="App">Header 1</div>;
+  return (
+    <div className="App">
+      <Category variant="liked" text="Liked Songs" />
+      <Category variant="recently" text="Recently Played" />
+      <Category variant="friends" text="Friends Played" />
+    </div>
+  );
 }
 
 export default App;
--- src/app/components/Category/Category.tsx
+++ src/app/components/Category/Category.tsx
@@ -1,3 +1,16 @@
+import { CategoryIcon } from '../CategoryIcon/CategoryIcon';
 import styles from './Category.module.scss';
 
-export const Category = () => {};
+type CategoryProps = {
+  text: string;
+  variant: 'liked' | 'recently' | 'friends';
+};
+
+export const Category = ({ text, variant }: CategoryProps) => {
+  return (
+    <div className={styles.categoryWrapper}>
+      <CategoryIcon variant={variant} />
+      <p className={styles.text}>{text}</p>
+    </div>
+  );
+};
