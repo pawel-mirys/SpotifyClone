58a2ac07ce65fac6b78b79f9844f04a618dd90fa
YoshiCity <pawel.mirys@gmail.com>
2022-04-21 18:28:25 +0200

    Correct paths, gradient names, clsx syntax

--- src/app/components/Category/Category.tsx
+++ src/app/components/Category/Category.tsx
@@ -14,13 +14,11 @@ export const Category = ({ name, text, iconStyle, className }: CategoryProps) =>
   return (
     <div className={styles.categoryWrapper}>
       <div
-        className={clsx(
-          className,
-          styles.iconWrapper,
-          iconStyle === 'blue' && styles.blueGradient,
-          iconStyle === 'purple' && styles.purpleGradient,
-          iconStyle === 'pink' && styles.pinkGradient,
-        )}
+        className={clsx(className, styles.iconWrapper, {
+          [styles.blueGradient]: iconStyle === 'blue',
+          [styles.purpleGradient]: iconStyle === 'purple',
+          [styles.pinkGradient]: iconStyle === 'pink',
+        })}
       >
         <Icon className={styles.icon} name={name} />
       </div>
