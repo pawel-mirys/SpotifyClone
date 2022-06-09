3edd599c31b5dccfb9039177d5cab53df426f196
YoshiCity <pawel.mirys@gmail.com>
2022-06-09 19:04:49 +0200

    Delete Card Container

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,21 +1,80 @@
-import { CardContainer, CardContainerProps } from './CardContainer';
+import clsx from 'clsx';
+import { useState } from 'react';
 
-type CardProps = {
-  cardType: CardContainerProps['cardType'];
-  title: CardContainerProps['title'];
-  description?: CardContainerProps['description'];
-  className?: CardContainerProps['className'];
-  coverName: CardContainerProps['coverName'];
+import { Cover, CoverProps } from 'ui/Cover/Cover';
+import { Icon } from 'ui/Icon/Icon';
+import { IconButton } from 'ui/IconButton/IconButton';
+import styles from './Card.module.scss';
+
+export type Card = {
+  cardType: 'personal' | 'playlist';
+  title: string;
+  description?: string;
+  coverName: CoverProps['name'];
+  className?: string;
 };
 
-export const Card = ({ cardType, title, description, className, coverName }: CardProps) => {
+export const Card = ({ cardType, title, description, className, coverName }: Card) => {
+  const [focus, setFocus] = useState(false);
+  const [follow, setFollow] = useState(false);
+
   return (
-    <CardContainer
-      cardType={cardType}
-      title={title}
-      description={description}
-      className={className}
-      coverName={coverName}
-    />
+    <>
+      <div
+        onClick={() => {
+          console.log(`Open playlist: ${title}`);
+        }}
+        onMouseEnter={() => {
+          setFocus(true);
+        }}
+        onMouseLeave={() => {
+          setFocus(false);
+        }}
+        className={clsx(className, styles.cardContainer, {
+          [styles.playlist]: cardType === 'playlist',
+          [styles.personal]: cardType === 'personal',
+        })}
+      >
+        <div className={styles.coverWrapper}>
+          <Cover name={coverName} />
+          {focus === true && (
+            <IconButton
+              className={styles.playButton}
+              onClick={() => {
+                console.log(`Run: ${title}`);
+              }}
+              children={<Icon name="play" />}
+            />
+          )}
+        </div>
+        <div
+          className={clsx(styles.titleWrapper, {
+            [styles.wrapperPlaylist]: cardType === 'playlist',
+            [styles.wrapperPersonal]: cardType === 'personal',
+          })}
+        >
+          <h4 className={styles.cardTitle}>{title}</h4>
+          {cardType === 'personal' && <p className={styles.cardDescription}>{description}</p>}
+          {cardType === 'playlist' && !follow && (
+            <IconButton
+              onClick={() => {
+                setFollow(true);
+                console.log(`Follow: ${title}`);
+              }}
+              children={<Icon className={styles.followIcon} name="follow" />}
+            />
+          )}
+          {cardType === 'playlist' && follow && (
+            <IconButton
+              onClick={() => {
+                setFollow(false);
+                console.log(`Unfollow: ${title}`);
+              }}
+              children={<Icon className={styles.followIcon} name="unfollow" />}
+            />
+          )}
+        </div>
+      </div>
+    </>
   );
 };
